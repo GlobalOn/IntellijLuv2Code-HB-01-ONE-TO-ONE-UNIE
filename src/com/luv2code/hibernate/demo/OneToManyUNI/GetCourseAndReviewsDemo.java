b903abd9679decabf2a364ffc6f4636e64dce783
GlobalOn <urkin961@gmail.com>
2021-11-29 18:17:03 +0300

    Part26-Hibernate-Advanced-Mappings-@OneToMany-Unidirectional
    Unidirectional one-to-many.

---
+++ src/com/luv2code/hibernate/demo/OneToManyUNI/GetCourseAndReviewsDemo.java
@@ -0,0 +1,45 @@
+package com.luv2code.hibernate.demo.OneToManyUNI;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCourseAndReviewsDemo {
+    public static void main(String[] args) {
+
+        //create session factory
+        SessionFactory sessionFactory = new Configuration().
+                configure("hibernate-one-to-many-uni.cfg.xml").
+                addAnnotatedClass(Instructor.class).
+                addAnnotatedClass(InstructorDetail.class).
+                addAnnotatedClass(Course.class).
+                addAnnotatedClass(Review.class).
+                buildSessionFactory();
+        //create session
+
+        try (sessionFactory; Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+
+            int theId = 10;
+
+            //get the course
+
+            Course course = session.get(Course.class, theId);
+
+            //print the course
+
+            System.out.println(course);
+
+            //print the course reviews
+            System.out.println(course.getReviews());
+
+            //commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        }
+    }
+}
--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -3,12 +3,16 @@ package com.luv2code.hibernate.demo.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -25,6 +29,10 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -55,6 +63,22 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    //add convenience method to add review
+    public void addReview(Review review) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+        reviews.add(review);
     }
 
     @Override
